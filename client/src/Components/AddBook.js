c90c500b01a168c4e98050eff1d25352d07d7b34
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-09-24 02:21:56 +0500

    update

--- client/src/Components/AddBook.js
+++ client/src/Components/AddBook.js
@@ -1,51 +1,3 @@
-// import React, { useState } from "react";
-
-// //ICONS
-// import { FiMinus } from "react-icons/fi";
-
-// // STYLES
-// import classes from "../styles/bookList.module.css";
-
-// const AddBook = () => {
-//   const [toggle, setToggle] = useState(false);
-
-//   const submit = (e) => {
-//     e.preventDefault();
-//   };
-
-//   return (
-//     <div className={`${toggle ? classes.addBookContainer : classes.addBookButton} ${classes.addBook}`}>
-//       {toggle ? (
-//         <div>
-//           <div className={classes.closeButton} onClick={() => setToggle(false)}>
-//             <FiMinus />
-//           </div>
-//           <form onSubmit={submit} className={classes.addBookForm}>
-//             <div className={classes.fields}>
-//               <input type='text' placeholder=' ' id='A1' />
-//               <label htmlFor='A1'>Book</label>
-//             </div>
-
-//             <div className={classes.fields}>
-//               <input type='text' placeholder=' ' id='A2' />
-//               <label htmlFor='A2'>Auther</label>
-//             </div>
-
-//             <div className={classes.fields}>
-//               <input type='text' placeholder=' ' id='A3' />
-//               <label htmlFor='A3'>Date</label>
-//             </div>
-//           </form>
-//         </div>
-//       ) : (
-//         <div onClick={() => setToggle(true)}>+</div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default AddBook;
-
 import React, { useState } from "react";
 
 //ICONS
@@ -55,35 +7,44 @@ import { FiMinus } from "react-icons/fi";
 import classes from "../styles/bookList.module.css";
 
 const AddBook = () => {
+  const [toggle, setToggle] = useState(false);
+
   const submit = (e) => {
     e.preventDefault();
   };
 
   return (
-    <div className={classes.addBook}>
-      <div className={classes.addBookButton} tabIndex='1'>
-        +
-      </div>
+    <div className={`${toggle ? classes.addBookContainer : classes.addBookButton} ${classes.addBook}`}>
+      {toggle ? (
+        <div>
+          <div className={classes.closeButton} onClick={() => setToggle(false)}>
+            <FiMinus />
+          </div>
+          <form onSubmit={submit} className={classes.addBookForm}>
+            <div className={classes.fields}>
+              <input type='text' placeholder=' ' id='A1' />
+              <label htmlFor='A1'>Book</label>
+            </div>
 
-      <div className={classes.addBookContainer} tabIndex='2'>
-        <div className={classes.closeButton} tabIndex='-1'>
-          <FiMinus />
+            <div className={classes.fields}>
+              <input type='text' placeholder=' ' id='A2' />
+              <label htmlFor='A2'>Auther</label>
+            </div>
+
+            <div className={classes.fields}>
+              <input type='text' placeholder=' ' id='A3' />
+              <label htmlFor='A3'>Date</label>
+            </div>
+          </form>
         </div>
-        <form onSubmit={submit} className={classes.addBookForm}>
-          <div className={classes.fields}>
-            <input type='text' placeholder=' ' id='A1' />
-            <label htmlFor='A1'>Book</label>
-          </div>
-          <div className={classes.fields}>
-            <input type='text' placeholder=' ' id='A2' />
-            <label htmlFor='A2'>Auther</label>
-          </div>
-          <div className={classes.fields}>
-            <input type='text' placeholder=' ' id='A3' />
-            <label htmlFor='A3'>Date</label>
-          </div>
-        </form>
-      </div>
+      ) : (
+        <div
+          onClick={() => {
+            setToggle(true);
+          }}>
+          +
+        </div>
+      )}
     </div>
   );
 };
